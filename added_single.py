448d474be48d0fac5b35b7868efe4a7031e699e1
Hansika-Prabodini <hansika@turintech.ai>
2025-09-08 14:52:25 +0530

    added single.py for the testing purpose

---
+++ added_single.py
@@ -0,0 +1,10 @@
+# added single.py for the testing purpose
+
+def single():
+    print("SingleForLoop")
+    print("-------------")
+
+    print(f"sum_range(10): {SingleForLoop.sum_range(10)}")
+    print(f"max_list([1, 2, 3]): {SingleForLoop.max_list([1, 2, 3])}")
+    print(f"sum_modulus(100, 3): {SingleForLoop.sum_modulus(100, 3)}")
+    print()
